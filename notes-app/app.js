fa55b2958a60165cf501f9261944f99486d1c57b
EduardoPicolo <eduardonpicolo@gmail.com>
2020-04-08 15:31:51 -0300

    Importing Chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,7 @@
+const chalk = require('chalk')
+const validator = require('validator')
 const getNotes = require('./notes')
 
 console.log(getNotes())
+
+console.log(chalk.green.bold.inverse("Success!"));
