3e073ee144cd37ec3d658b9780b2a1140d120e28
EduardoPicolo <eduardonpicolo@gmail.com>
2020-04-09 06:46:34 -0300

    Definindo comandos do yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,10 +3,55 @@ const yargs = require('yargs')
 const validator = require('validator')
 const getNotes = require('./notes')
 
-const command = process.argv[2]
+// Create add command
+yargs.command({
+   command: 'add',
+   describe: 'Add a new note',
+   builder: {
+      title: {
+         describe: 'Note title',
+         demandOption: true,
+         type: 'string',
+      },
+      body: {
+         describe: 'Note body',
+         demandOption: true,
+         type: 'string',
+      },
+   },
+   handler: function(argv) {
+      console.log('Title: ' + argv.title)
+      console.log('Body: ' + argv.body)
+   }
+})
 
-if (command === 'add') {
-   console.log('Adding note!')
-} else if (command === 'remove') {
-   console.log('Removing note!')
-}
+// Create remove command
+yargs.command({
+   command: 'remove',
+   describe: 'Remove a note',
+   handler: () => {
+      console.log('Removing the note')
+   }
+})
+
+// Create read command
+yargs.command({
+   command: 'read',
+   describe: 'Read a note',
+   handler: (argv) => {
+      console.log('Reading the note...', argv)
+   }
+})
+
+// Create list command
+yargs.command({
+   command: 'list',
+   describe: 'List all notes',
+   handler: () => {
+      console.log('Listing notes')
+   }
+})
+
+
+// console.log(yargs.argv)
+yargs.parse()
