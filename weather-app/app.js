70a9d65902fc5bb363eeb67340ffaf1fa34fa787
EduardoPicolo <eduardonpicolo@gmail.com>
2020-04-09 16:02:39 -0300

    Organizando codigo

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,21 +1,13 @@
 const request = require('request')
 const geocode = require('./utils/geocode')
-
-const weatherURL = 'http://api.weatherstack.com/current?access_key=e40f5821de07d2f54416b549b4eccdaa&query=37.8267,-122.4233'
-
-// request({ url: weatherURL, json: true }, (error, response) => {
-//    if (error) {
-//       console.log('Unable to connect to weather service!')
-//    } else if (response.body.error) {
-//       console.log(response.body.error)
-//    } else {
-//       const temp = response.body.current.temperature
-//       const feelsLikeTemp = response.body.current.feelslike
-//       console.log('It is currently ' + temp + ' degress out. It feels like ' + feelsLikeTemp + ' degress out.')
-//    }
-// })
+const forecast = require('./utils/forecast')
 
 geocode('Philadelphia New York', (error, data) => {
    console.log('Error:', error)
    console.log('Data:', data)
 })
+
+forecast(-75.7088, 44.1545, (error, data) => {
+   console.log('Error:', error)
+   console.log('Data', data)
+ })
---
+++ weather-app/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+const forecast = (longitude, latitude, done) => {
+   const url = `http://api.weatherstack.com/current?access_key=e40f5821de07d2f54416b549b4eccdaa&query=${latitude},${longitude}`
+
+   request({ url, json: true }, (error, response) => {
+      if (error) {
+         done('Unable to connect to weather service!')
+      } else if (response.body.error) {
+         done(response.body.error)
+      } else {
+         const temp = response.body.current.temperature
+         const feelsLikeTemp = response.body.current.feelslike
+         const data = `It is currently ${temp} degress out. It feels like ${feelsLikeTemp} degress out.`
+         done(null, data)
+      }
+   })
+}
+
+module.exports = forecast
