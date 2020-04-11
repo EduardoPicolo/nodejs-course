08de14fd7633614fc67ace3c2e882c8be0d5b90a
EduardoPicolo <eduardonpicolo@gmail.com>
2020-04-11 10:31:57 -0300

    50. 404 Pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -46,6 +46,20 @@ app.get('/weather', (req, res) => {
    })
 })
 
+app.get('/help/*', (req, res) => {
+   res.render('404', {
+      title: '404,',
+      error: 'Help article not found'
+   })
+})
+
+app.get('*', (req, res) => {
+   res.render('404', {
+      title: 404,
+      error: 'Page not found'
+   })
+})
+
 app.listen(3000, () => {
    console.log('Server is running on port 3000.')
 })
