ed5f37402435c9727753e1a30f3bad379e7ab428
EduardoPicolo <eduardonpicolo@gmail.com>
2020-04-11 09:20:48 -0300

    46. Serving up CSS, JS and Images

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,9 +1,6 @@
 const path = require('path')
 const express = require('express')
 
-// console.log(__dirname)
-// console.log(path.join(__dirname, '../public'))
-
 const app = express()
 
 const publicDirPath = path.join(__dirname, '../public')
