11b2f8667254bd47e6417342cab2a12a67f8e059
EduardoPicolo <eduardonpicolo@gmail.com>
2020-04-11 09:45:38 -0300

    47. Dynamic pages with templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,10 +2,31 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-
 const publicDirPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirPath))
+
+app.get('/', (req, res) => {
+   res.render('index', {
+      title: 'Weather',
+      name: 'Eduardo'
+   })
+})
+
+app.get('/about', (req, res) => {
+   res.render('about', {
+      title: 'About',
+      name: 'Eduardo'
+   })
+})
+
+app.get('/help', (req, res) => {
+   res.render('help', {
+      title: 'Help',
+      helpText: 'This is some helpful text.'
+   })
+})
 
 app.get('/weather', (req, res) => {
    res.send({
