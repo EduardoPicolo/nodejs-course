91a4ad94bcdab4f82e3c6a42f2707a1ee707e21f
EduardoPicolo <eduardonpicolo@gmail.com>
2020-04-12 10:26:34 -0300

    67. DEplying Node.js to Heroku

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,6 +5,7 @@ const hbs = require('hbs')
 const { geocode, forecast } = require('./utils/index')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for express config
 const publicDirPath = path.join(__dirname, '../public')
@@ -76,6 +77,6 @@ app.get('*', (req, res) => {
    })
 })
 
-app.listen(3000, () => {
-   console.log('Server is running on port 3000.')
+app.listen(port, () => {
+   console.log('Server is running on port ' + port)
 })
