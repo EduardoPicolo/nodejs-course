e5b91ee2d3fd7a19d67c392fc164cfd27045b55e
EduardoPicolo <eduardonpicolo@gmail.com>
2020-04-13 14:14:53 -0300

    81. Deleting documents

--- task-manager-app/mongodb.js
+++ task-manager-app/mongodb.js
@@ -17,28 +17,19 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
    const db = client.db(databaseName)
 
-   // db.collection('users').updateOne({
-   //    _id: new ObjectID("5e94744ca7c1e04a90dc0fd6")
-   // }, {
-   //    $inc: {
-   //       age: 1
-   //    }
+   // db.collection('users').deleteMany({
+   //    age: 19,
    // }).then((result) => {
    //    console.log(result)
    // }).catch((error) => {
    //    console.log(error)
    // })
 
-   db.collection('tasks').updateMany({
-      completed: false
-   }, {
-      $set: {
-         completed: true
-      }
+   db.collection('tasks').deleteOne({
+      description: 'second task',
    }).then((result) => {
       console.log(result)
    }).catch((error) => {
       console.log(error)
    })
-
 })
