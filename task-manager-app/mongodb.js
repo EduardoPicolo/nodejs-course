2430b4a87a05be51b802f91d449c0b82dbdd1b78
EduardoPicolo <eduardonpicolo@gmail.com>
2020-04-13 13:53:26 -0300

    80. Updating documents

--- task-manager-app/mongodb.js
+++ task-manager-app/mongodb.js
@@ -17,28 +17,28 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
    const db = client.db(databaseName)
 
-   // db.collection('users').findOne({ _id: new ObjectID("5e94764c0ead664bd9a265fe") }, (error, user) => {
-   //    if (error) return console.log(error)
-   //    console.log(user)
+   // db.collection('users').updateOne({
+   //    _id: new ObjectID("5e94744ca7c1e04a90dc0fd6")
+   // }, {
+   //    $inc: {
+   //       age: 1
+   //    }
+   // }).then((result) => {
+   //    console.log(result)
+   // }).catch((error) => {
+   //    console.log(error)
    // })
 
-   // db.collection('users').find({ age: 19 }).toArray((error, users) => {
-   //    if (error) return console.log(error)
-   //    console.log(users)
-   // })
-
-   // db.collection('users').find({ age: 19 }).count((error, count) => {
-   //    if (error) return console.log(error)
-   //    console.log(count)
-   // })
-
-   db.collection('tasks').findOne({ _id: new ObjectID("5e9477e3cc136a4cfc9c3ca3") }, (error, task) => {
-      if (error) return console.log(error)
-      console.log(task)
+   db.collection('tasks').updateMany({
+      completed: false
+   }, {
+      $set: {
+         completed: true
+      }
+   }).then((result) => {
+      console.log(result)
+   }).catch((error) => {
+      console.log(error)
    })
 
-   db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
-      if(error) return console.log(error)
-      console.log(tasks)
-   })
 })
