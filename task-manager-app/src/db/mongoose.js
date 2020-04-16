a8c8559e54b0285c6bc1d1e117dc2f8828b0911d
EduardoPicolo <eduardonpicolo@gmail.com>
2020-04-16 18:32:26 -0300

    90. Resouce creation endpoints: Part 2

--- task-manager-app/src/db/mongoose.js
+++ task-manager-app/src/db/mongoose.js
@@ -4,16 +4,3 @@ moongose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
    useNewUrlParser: true,
    useCreateIndex: true
 })
-
-
-const Task = moongose.model('Task', {
-   description: {
-      type: String,
-      required: true,
-      trim: true
-   },
-   completed: {
-      type: Boolean,
-      default: false
-   },
-})
--- task-manager-app/src/index.js
+++ task-manager-app/src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 require('./db/mongoose')
+
 const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -10,10 +12,19 @@ app.use(express.json())
 app.post('/users', (req, res) => {
    const user = new User(req.body)
    user.save()
-      .then(data => res.send(data))
+      .then(data => {
+         res.status(201).send(data)
+      })
       .catch(error => {
          res.status(400).send(error)
       })
+})
+
+app.post('/tasks', (req, res) => {
+   const task = new Task(req.body)
+   task.save()
+      .then(data => res.status(201).send(data))
+      .catch(error => res.status(400).send(error))
 })
 
 app.listen(port, () => {
