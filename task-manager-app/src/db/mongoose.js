ffb53ca56ad2f22aeb9e94020110077b22e87ef1
EduardoPicolo <eduardonpicolo@gmail.com>
2020-04-16 16:50:16 -0300

    86. Data validation and sanitization: Part 1

--- task-manager-app/src/db/mongoose.js
+++ task-manager-app/src/db/mongoose.js
@@ -1,4 +1,5 @@
-const moongose= require('mongoose')
+const moongose = require('mongoose')
+const validator = require('validator')
 
 moongose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
    useNewUrlParser: true,
@@ -7,15 +8,35 @@ moongose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = moongose.model('User', {
    name: {
-      type: String
+      type: String,
+      required: true,
+      trim: true,
+   },
+   email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+         if (!validator.isEmail(value)) {
+            throw new Error('Email is invalid')
+         }
+      },
    },
    age: {
-      type: Number
-   }
+      type: Number,
+      default: 0,
+      validate(value) {
+         if (value < 0) {
+            console.log('error')
+            throw new Error('Age must be a positive number')
+         }
+      },
+   },
 })
 
-// const me = new User({name: 'Eduardo', age: 19})
-// me.save().then((data) => {console.log(data)}).catch((error) => {console.log(error)})
+const me = new User({ name: '    Picolo  ', email: 'picolo@mail.com   ' })
+me.save().then((data) => { console.log(data) }).catch((error) => { console.log(error) })
 
 const Task = moongose.model('Task', {
    description: {
@@ -26,5 +47,5 @@ const Task = moongose.model('Task', {
    },
 })
 
-const myTask = new Task({description: 'my first task', completed: true})
-myTask.save().then(data => console.log(data)).catch(error => console.log(error))
+// const myTask = new Task({description: 'my first task', completed: true})
+// myTask.save().then(data => console.log(data)).catch(error => console.log(error))
