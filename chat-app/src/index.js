1afa8c57ea2e0432d295fb831d149682cfc1b562
EduardoPicolo <eduardonpicolo@gmail.com>
2020-05-03 05:07:32 -0300

    167. Socket.io rooms

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -18,15 +18,19 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) => {
    console.log('New WebSocket connection')
 
-   socket.emit('message', generateMessage('Welcome!'))
-   socket.broadcast.emit('message', generateMessage('A new user has joined'))
+   socket.on('join', ({ username, room }) => {
+      socket.join(room)
+
+      socket.emit('message', generateMessage('Welcome!'))
+      socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`))
+   })
 
    socket.on('sendMessage', (msg, callback) => {
       const filter = new Filter()
 
       if (filter.isProfane(msg)) return callback('Profanity is not allowed')
 
-      io.emit('message', generateMessage(msg))
+      io.to('the best room').emit('message', generateMessage(msg))
       callback()
    })
 
