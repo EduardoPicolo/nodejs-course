e1c63791cf2c5687a6fdd8cfcc65bcaa4684a6fc
EduardoPicolo <eduardonpicolo@gmail.com>
2020-05-03 00:22:43 -0300

    158. Sharing location

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -11,3 +11,12 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 
    socket.emit('sendMessage', msg)
 })
+
+document.querySelector('#send-location').addEventListener('click', () => {
+   if (!navigator.geolocation) return alert('Geolocation is not supported by your browser')
+
+   navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords
+      socket.emit('sendLocation', { latitude, longitude })
+   })
+})
