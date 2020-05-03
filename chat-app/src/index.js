17d99f6b37c33c86cacf9fdcd96c7ab5079b7035
EduardoPicolo <eduardonpicolo@gmail.com>
2020-05-02 22:47:35 -0300

    155. Socket.io events

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -5,18 +5,26 @@ const path = require('path')
 
 const app = express()
 const server = http.createServer(app)
-const port = process.env.PORT
-
 const io = socketio(server)
 
-io.on('connection', () => {
-   console.log('New WebSocket connection')
-})
-
+const port = process.env.PORT
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.json())
 app.use(express.static(publicDirectoryPath))
+
+let count = 0
+
+io.on('connection', (socket) => {
+   console.log('New WebSocket connection')
+
+   socket.emit('countUpdated', count)
+
+   socket.on('increment', () => {
+      count++
+      io.emit('countUpdated', count)
+   })
+})
 
 server.listen(port, () => {
    console.log('Server is up on port ' + port)
